b7efaac1872daa8757eb9ebddcb224bfbd72b052
ViniGhiraldi <vinighiraldi198@gmail.com>
2024-02-09 13:35:52 -0300

    fix: fixed logo bug

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,12 +2,13 @@ import { Cards } from '@/components/cards';
 import { InputMain } from '@/components/input-main';
 import { NewNoteCard } from '@/components/new-note-card';
 import Image from 'next/image';
+import logo from '@/../public/nlwexpert.svg';
 
 export default function Home() {
   return (
     <main className="bg-slate-900 min-h-screen text-slate-400 pt-16 pb-10 flex items-start justify-center px-5">
       <div className="max-w-6xl w-full flex flex-col gap-6">
-        <Image src='/nlwexpert.svg' height={24} width={124.5} alt='logo' />
+        <Image src={logo} height={24} width={124.5} alt='logo' />
         <InputMain/>
         <div className='w-full h-px bg-slate-700'></div>
         <div className='grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6'>
