0a25c0685f0706d0abcd60f615721cf40c5a69f4
Vinícius Correia Ghiraldi <99772856+ViniGhiraldi@users.noreply.github.com>
2024-02-09 13:26:02 -0300

    Update page.tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -7,7 +7,7 @@ export default function Home() {
   return (
     <main className="bg-slate-900 min-h-screen text-slate-400 pt-16 pb-10 flex items-start justify-center px-5">
       <div className="max-w-6xl w-full flex flex-col gap-6">
-        <Image src='/logo.svg' height={24} width={124.5} alt='logo' />
+        <Image src='/nlwexpert.svg' height={24} width={124.5} alt='logo' />
         <InputMain/>
         <div className='w-full h-px bg-slate-700'></div>
         <div className='grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6'>
